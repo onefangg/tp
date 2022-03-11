6206db92e885b8e53bc5cb8979fd57dd3b276e6f
Gerald Tan <tanjunweigerald1997@gmail.com>
2022-03-11 11:29:11 +0800

    create test for delete order command add files

--- src/test/java/seedu/address/testutil/TypicalOrders.java
+++ src/test/java/seedu/address/testutil/TypicalOrders.java
@@ -1,6 +1,11 @@
 package seedu.address.testutil;
 
+import seedu.address.model.AddressBook;
 import seedu.address.model.order.Order;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class TypicalOrders {
     public static final Order EMILY = new OrderBuilder().withName("Emily Lee")
@@ -13,4 +18,25 @@ public class TypicalOrders {
             .withPhone("94241423")
             .withDetails("1xchocolatecake").build();
 
+    public static final Order JERRY = new OrderBuilder().withName("Jerry Mouse")
+            .withAddress("55 Ang Mo Kio Avenue 38")
+            .withPhone("91029382")
+            .withDetails("1xjerryfavouritecheesecake").build();
+
+    private TypicalOrders() {} // prevents instantiation
+
+    /**
+     * Returns an {@code AddressBook} with all the typical orders.
+     */
+    public static AddressBook getTypicalAddressBookOrders() {
+        AddressBook ab = new AddressBook();
+        for (Order order : getTypicalOrders()) {
+            ab.addOrder(order);
+        }
+        return ab;
+    }
+
+    public static List<Order> getTypicalOrders() {
+        return new ArrayList<>(Arrays.asList(EMILY, SIMON, JERRY));
+    }
 }
