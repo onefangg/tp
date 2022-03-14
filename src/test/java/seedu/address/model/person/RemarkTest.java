91a4ea8215a45eee5bffbbced0a02a0ded7db1dc
Gerald Tan <tanjunweigerald1997@gmail.com>
2022-03-14 17:00:19 +0800

    Edit RemarkTest

--- src/test/java/seedu/address/model/person/RemarkTest.java
+++ src/test/java/seedu/address/model/person/RemarkTest.java
@@ -1,11 +1,9 @@
-package seedu.remark.model.person;
+package seedu.address.model.person;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.testutil.Assert.assertThrows;
 
 import org.junit.jupiter.api.Test;
-import seedu.address.model.person.Remark;
 
 public class RemarkTest {
 
@@ -19,10 +17,10 @@ public class RemarkTest {
         // null remark
         assertThrows(NullPointerException.class, () -> Remark.isValidRemark(null));
 
-        // valid remarkes
-        assertTrue(Remark.isValidRemark("Blk 456, Den Road, #01-355"));
+        // valid remarks
+        assertTrue(Remark.isValidRemark("In love with sky diving"));
         assertTrue(Remark.isValidRemark("-")); // one character
         assertTrue(Remark.isValidRemark("")); // empty remark
-        assertTrue(Remark.isValidRemark("Leng Inc; 1234 Market St; San Francisco CA 2349879; USA")); // long remark
+        assertTrue(Remark.isValidRemark("Waterfall Enthusiast")); // long remark
     }
 }
