f6dbd8673acfbcd77f5edc53fa412068efe87fba
onefangg <onefangg@gmail.com>
2022-03-17 20:59:40 +0800

    Fix checkstyle issues with AddOrderCommandParserTest * Accidental Commit of file - will resolve separately in another issue

--- src/test/java/seedu/address/logic/parser/AddOrderCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/AddOrderCommandParserTest.java
@@ -1,17 +1,17 @@
 package seedu.address.logic.parser;
 
-import org.junit.jupiter.api.Test;
-import seedu.address.logic.commands.AddOrderCommand;
-import seedu.address.model.order.Order;
-import seedu.address.testutil.OrderBuilder;
-
 import static seedu.address.logic.commands.CommandTestUtil.ADDRESS_DESC_AMY;
-import static seedu.address.logic.commands.CommandTestUtil.DETAILS_DESC_AMY;
 import static seedu.address.logic.commands.CommandTestUtil.EMAIL_DESC_AMY;
 import static seedu.address.logic.commands.CommandTestUtil.NAME_DESC_AMY;
 import static seedu.address.logic.commands.CommandTestUtil.PHONE_DESC_AMY;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
 import static seedu.address.testutil.TypicalOrders.EMILY;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.address.logic.commands.AddOrderCommand;
+import seedu.address.model.order.Order;
+import seedu.address.testutil.OrderBuilder;
 
 public class AddOrderCommandParserTest {
     private AddOrderCommandParser parser = new AddOrderCommandParser();
@@ -20,7 +20,8 @@ public class AddOrderCommandParserTest {
     public void parse_allFieldsPresent_success() {
         Order expectedOrder = new OrderBuilder(EMILY).build();
 
-        assertParseSuccess(parser, NAME_DESC_AMY + PHONE_DESC_AMY + ADDRESS_DESC_AMY + EMAIL_DESC_AMY + DETAILS_DESC_AMY
-                , new AddOrderCommand(expectedOrder));
+        assertParseSuccess(parser,
+                NAME_DESC_AMY + PHONE_DESC_AMY + ADDRESS_DESC_AMY + EMAIL_DESC_AMY,
+                new AddOrderCommand(expectedOrder));
     }
 }
