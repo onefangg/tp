c3e7b885f3bdaa6d5eb26af6e3be59558fdaec1a
onefangg <onefangg@gmail.com>
2022-03-18 19:16:26 +0800

    Remove superfluous commit of arePrefixesPresent method

--- src/main/java/seedu/address/logic/parser/ParserUtil.java
+++ src/main/java/seedu/address/logic/parser/ParserUtil.java
@@ -140,11 +140,4 @@ public class ParserUtil {
         return tagSet;
     }
 
-    /**
-     * Returns true if none of the prefixes contains empty {@code Optional} values in the given
-     * {@code ArgumentMultimap}.
-     */
-    public static boolean arePrefixesPresent(ArgumentMultimap argumentMultimap, Prefix... prefixes) {
-        return Stream.of(prefixes).allMatch(prefix -> argumentMultimap.getValue(prefix).isPresent());
-    }
 }
