f892433a3a5c7dcad4be80bc1482459ffd968aa2
darshan152 <59141335+darshan152@users.noreply.github.com>
2022-03-29 09:56:10 +0800

    Fix checkstyle error

--- src/test/java/seedu/address/logic/commands/DeletePersonCommandTest.java
+++ src/test/java/seedu/address/logic/commands/DeletePersonCommandTest.java
@@ -9,7 +9,6 @@ import static seedu.address.testutil.TypicalAddressBook.getTypicalAddressBook;
 import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
 import static seedu.address.testutil.TypicalIndexes.INDEX_FOURTH_PERSON;
 import static seedu.address.testutil.TypicalIndexes.INDEX_SECOND_PERSON;
-import static seedu.address.testutil.TypicalPersons.getTypicalAddressBookPersons;
 
 import org.junit.jupiter.api.Test;
 
