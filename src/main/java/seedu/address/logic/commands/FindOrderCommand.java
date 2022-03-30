06d1b6afcb312745874e519e793e964bdc96a18a
onefangg <onefangg@gmail.com>
2022-03-31 00:03:40 +0800

    Add message usage

--- src/main/java/seedu/address/logic/commands/FindOrderCommand.java
+++ src/main/java/seedu/address/logic/commands/FindOrderCommand.java
@@ -11,6 +11,7 @@ public abstract class FindOrderCommand extends Command {
             + ": Finds all orders based on attributes (name/phone/details/collectionType) "
             + "whose attributes contain any of the specified keywords (case-insensitive)"
             + " and displays them as a list with index numbers.\n"
+            + "The command does not allow multiple attributes to be filtered at the same time.\n"
             + "Parameters: (n|p|d|m)/ KEYWORD [MORE_KEYWORDS]...\n"
             + "Example: " + COMMAND_WORD + " n/ alice bob charlie";
 }
