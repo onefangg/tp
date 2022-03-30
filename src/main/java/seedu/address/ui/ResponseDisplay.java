a44c54b0bafc5c4095b042572723ec1eb7d793eb
Pun Hui Min <punhuimin99@gmail.com>
2022-03-30 21:56:51 +0800

    Resolve checkstyle error

--- src/main/java/seedu/address/ui/ResponseDisplay.java
+++ src/main/java/seedu/address/ui/ResponseDisplay.java
@@ -4,7 +4,6 @@ import static java.util.Objects.requireNonNull;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.TextArea;
-import javafx.scene.image.ImageView;
 import javafx.scene.layout.Region;
 
 /**
