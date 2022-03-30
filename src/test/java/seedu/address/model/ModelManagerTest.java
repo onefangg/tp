aaf8c4091b20b0b0ea23b2c98cf1c73f1b273aa6
darshan152 <59141335+darshan152@users.noreply.github.com>
2022-03-30 16:45:23 +0800

    Add tests for new methods

--- src/test/java/seedu/address/model/ModelManagerTest.java
+++ src/test/java/seedu/address/model/ModelManagerTest.java
@@ -94,6 +94,21 @@ public class ModelManagerTest {
     }
 
     @Test
+    public void getFilteredOrderList_modifyList_throwsUnsupportedOperationException() {
+        assertThrows(UnsupportedOperationException.class, () -> modelManager.getFilteredOrderList().remove(0));
+    }
+
+    @Test
+    public void getPersonList_modifyList_throwsUnsupportedOperationException() {
+        assertThrows(UnsupportedOperationException.class, () -> modelManager.getPersonList().remove(0));
+    }
+
+    @Test
+    public void getOrderList_modifyList_throwsUnsupportedOperationException() {
+        assertThrows(UnsupportedOperationException.class, () -> modelManager.getOrderList().remove(0));
+    }
+
+    @Test
     public void equals() {
         AddressBook addressBook = new AddressBookBuilder().withPerson(ALICE).withPerson(BENSON).build();
         AddressBook differentAddressBook = new AddressBook();
