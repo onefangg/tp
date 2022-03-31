6077a5e109c42bd8cee409dddd10395e875ef77d
Gerald Tan <tanjunweigerald1997@gmail.com>
2022-03-31 14:56:35 +0800

    Change Remark to being an optional argument for addOrder Change Remark to being an optional argument for addPerson Edit Message Usage for findo

--- src/main/java/seedu/address/logic/commands/FindOrderCommand.java
+++ src/main/java/seedu/address/logic/commands/FindOrderCommand.java
@@ -8,10 +8,10 @@ public abstract class FindOrderCommand extends Command {
     public static final String COMMAND_WORD = "findo";
 
     public static final String MESSAGE_USAGE = COMMAND_WORD
-            + ": Finds all orders based on attributes (name/phone/details/collectionType/remark) "
+            + ": Finds all orders based on attributes (name/phone/details/collection_type/remark) "
             + "whose attributes contain any of the specified keywords (case-insensitive)"
             + " and displays them as a list with index numbers.\n"
             + "The command does not allow multiple attributes to be filtered at the same time.\n"
             + "Parameters: (n|p|d|m|r)/ KEYWORD [MORE_KEYWORDS]...\n"
-            + "Example: " + COMMAND_WORD + " n/ alice bob charlie";
+            + "Example: " + COMMAND_WORD + " n/alice bob charlie";
 }
